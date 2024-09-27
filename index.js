b65dfc905f7ae9776c159b724fdab23badafb396
joaosilvr <joaovitor.dasilvaramos@gmail.com>
2024-09-26 21:42:01 -0300

    aprendendo comandos básicos Javascript

---
+++ index.js
@@ -0,0 +1,27 @@
+
+
+/*
+comentario multi linha
+*/
+
+
+// comentario normal
+
+// alert('oii')
+
+
+// declarando variável
+let x = 'Rodrigo'
+
+console.log('ver o que esta no x', x);
+
+
+
+let nome = prompt('qual é o seu nome?')
+alert('seja bem vindo ' + nome +'!')
+
+
+// imprimir console
+console.log('qualquer coisa');
+
+
